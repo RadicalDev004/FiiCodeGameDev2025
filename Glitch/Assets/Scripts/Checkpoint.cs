836c494c6745e4acda2df889e23933bf282b767e
MTAxD12 <andrei.foco@yahoo.com>
2025-04-08 02:04:28 +0300

    checkpoint uri lucioase + sunete

--- Glitch/Assets/Scripts/Checkpoint.cs
+++ Glitch/Assets/Scripts/Checkpoint.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
+﻿using Pixelplacement;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Checkpoint : MonoBehaviour
 {
     public Button checkpointButton;
+    public GameObject checkpointObj;
     public Vector3 teleportLocation;
+
+    private void Start()
+    {
+        if (checkpointObj != null)
+        {
+            Vector3 finalPos = checkpointObj.transform.localPosition + new Vector3(0, -1f, 0);
+
+            checkpointObj.transform.localPosition = finalPos;
+            checkpointObj.transform.localScale = Vector3.one;
+            checkpointObj.transform.GetChild(0).GetComponent<Light>().enabled = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            StartCoroutine(hoverCheckpoint());
+
             MapManager mapController = FindObjectOfType<MapManager>();
             if (mapController != null)
             {
@@ -25,5 +41,16 @@ public class Checkpoint : MonoBehaviour
 
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator hoverCheckpoint()
+    {
+        Vector3 finalPos = checkpointObj.transform.localPosition + new Vector3(0, 1f, 0);
+        checkpointObj.transform.GetChild(0).GetComponent<Light>().enabled = true;
+
+        Tween.LocalPosition(checkpointObj.transform, finalPos, 3, 0, Tween.EaseInOut);
+        Tween.LocalScale(checkpointObj.transform, new Vector3(20f, 20f, 20f), 3, 0, Tween.EaseInOut);
+
+        yield return new WaitForSeconds(2f);
     }
 }
